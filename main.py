df01df3c8a2d2e2aa8f936039439bc71cc5d24c0
Jon Tamir <jtamir@eecs.berkeley.edu>
2019-10-04 16:53:44 -0700

    add argparse hyperparams and test on MoDL

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 #!/usr/bin/env python
 
-import os
+from test_tube import Experiment, HyperOptArgumentParser
+from pytorch_lightning import Trainer
 
-import pytorch_lightning as pl
-import test_tube
+import os
+import argparse
 
 #from deepinpy.models.mcmri import mcmri
 from deepinpy.recons.cgsense.cgsense import CGSense
+from deepinpy.recons.modl.modl import MoDL
 
-M = CGSense(l2lam=0.001)
-exp = test_tube.Experiment(save_dir=os.getcwd())
+#torch.backends.cudnn.enabled = True
 
+def main_train(hparams, gpu_ids=None):
+    print(hparams)
+    #M = CGSense(l2lam=hparams.l2lam_init, step=hparams.step)
+    M = MoDL(l2lam=hparams.l2lam_init, step=hparams.step, num_unrolls=hparams.num_unrolls)
+    exp = Experiment(save_dir=os.getcwd())
 
-#trainer = pl.Trainer(experiment=exp, max_nb_epochs=1, train_percent_check=.1)
-#trainer = pl.Trainer(experiment=exp, max_nb_epochs=100, gpus=[2, 3], distributed_backend='dp')
-trainer = pl.Trainer(experiment=exp, max_nb_epochs=100, gpus=[3])
-trainer.fit(M)
+    #trainer = Trainer(experiment=exp, max_nb_epochs=1, train_percent_check=.1)
+    #trainer = Trainer(experiment=exp, max_nb_epochs=100, gpus=[2, 3], distributed_backend='dp')
+    print('gpu ids are', gpu_ids)
+    trainer = Trainer(experiment=exp, max_nb_epochs=100, gpus=gpu_ids)
+    #trainer = Trainer(experiment=exp, max_nb_epochs=10)
+
+    trainer.fit(M)
+
+if __name__ == '__main__':
+    usage_str = 'usage: %(prog)s [options]'
+    description_str = 'modl-based optimization'
+
+    parser = HyperOptArgumentParser(usage=usage_str, description=description_str, formatter_class=argparse.ArgumentDefaultsHelpFormatter, strategy='random_search')
+
+    parser.add_argument('--num_unrolls', action='store', dest='num_unrolls', type=int, help='number of unrolls', default=4)
+    parser.opt_range('--step', type=float, dest='step', default=.001, help='step size/learning rate', tunable=True, low=.0001, high=.1)
+    parser.add_argument('--l2lam_init', action='store', type=float, dest='l2lam_init', default=.001, help='initial l2 regularization')
+
+    hparams = parser.parse_args()
+
+    #hparams.optimize_parallel_gpu(main_train, gpu_ids=['2', '3'], max_nb_trials=10)
+    #hparams.optimize_parallel_cpu(main_train, nb_trials=20, nb_workers=2)
+    main_train(hparams, gpu_ids=[3])
