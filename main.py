2bca5e903af1500288e034834720b5f420098e48
Jon Tamir <jtamir@eecs.berkeley.edu>
2019-10-05 21:33:18 -0700

    name hparams to args

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ from deepinpy.recons.modl.modl import MoDL
 
 #torch.backends.cudnn.enabled = True
 
-def main_train(hparams, gpu_ids=None):
-    print(hparams)
-    #M = CGSense(l2lam=hparams.l2lam_init, step=hparams.step)
-    M = MoDL(l2lam=hparams.l2lam_init, step=hparams.step, num_unrolls=hparams.num_unrolls)
+def main_train(args, gpu_ids=None):
+    #print(args)
+    #M = CGSense(l2lam=args.l2lam_init, step=args.step)
+    M = MoDL(l2lam=args.l2lam_init, step=args.step, num_unrolls=args.num_unrolls)
     exp = Experiment(save_dir=os.getcwd())
 
     #trainer = Trainer(experiment=exp, max_nb_epochs=1, train_percent_check=.1)
@@ -44,8 +44,8 @@ if __name__ == '__main__':
     parser.add_argument('--l2lam_init', action='store', type=float, dest='l2lam_init', default=.001, help='initial l2 regularization')
     parser.add_argument('--recon', action='store', type=str, dest='recon', default='cgsense', help='reconstruction method')
 
-    hparams = parser.parse_args()
+    args = parser.parse_args()
 
-    #hparams.optimize_parallel_gpu(main_train, gpu_ids=['2', '3'], max_nb_trials=10)
-    #hparams.optimize_parallel_cpu(main_train, nb_trials=20, nb_workers=2)
-    main_train(hparams, gpu_ids=[3])
+    #args.optimize_parallel_gpu(main_train, gpu_ids=['2', '3'], max_nb_trials=10)
+    #args.optimize_parallel_cpu(main_train, nb_trials=20, nb_workers=2)
+    main_train(args, gpu_ids=[args.gpu])
