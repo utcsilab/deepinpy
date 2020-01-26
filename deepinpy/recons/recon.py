60a4264005f077d284cd6d324d87040c9374f07f
Jon Tamir <jtamir@eecs.berkeley.edu>
2020-01-26 14:33:22 -0600

    summarywrite warning for spaces

--- deepinpy/recons/recon.py
+++ deepinpy/recons/recon.py
@@ -97,16 +97,16 @@ class Recon(pl.LightningModule):
 
                 myim = torch.tensor(np.stack((np.abs(_x_hat), np.angle(_x_hat)), axis=0))[:, None, ...]
                 grid = make_grid(myim, scale_each=True, normalize=True, nrow=8, pad_value=10)
-                self.logger.experiment.add_image('2. train_prediction', grid, self.current_epoch)
+                self.logger.experiment.add_image('2_train_prediction', grid, self.current_epoch)
 
                 if self.current_epoch == 0:
                     myim = torch.tensor(np.stack((np.abs(_x_gt), np.angle(_x_gt)), axis=0))[:, None, ...]
                     grid = make_grid(myim, scale_each=True, normalize=True, nrow=8, pad_value=10)
-                    self.logger.experiment.add_image('1. ground_truth', grid, 0)
+                    self.logger.experiment.add_image('1_ground_truth', grid, 0)
 
                     myim = torch.tensor(np.stack((np.abs(_x_adj), np.angle(_x_adj)), axis=0))[:, None, ...]
                     grid = make_grid(myim, scale_each=True, normalize=True, nrow=8, pad_value=10)
-                    self.logger.experiment.add_image('0. input', grid, 0)
+                    self.logger.experiment.add_image('0_input', grid, 0)
 
 
         loss = self.loss_fun(x_hat, imgs)
