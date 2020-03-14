4a9d5de4d7b4f68123b622dbb299c2ed8f7f4c74
Jon Tamir <jtamir@utexas.edu>
2020-03-14 00:55:10 -0500

    work on unittest code rot

--- deepinpy/tests/test_recons.py
+++ deepinpy/tests/test_recons.py
@@ -10,6 +10,7 @@ from pytorch_lightning import Trainer
 import argparse
 
 from deepinpy.recons import CGSenseRecon, MoDLRecon, ResNetRecon, DeepBasisPursuitRecon
+from deepinpy.forwards import MultiChannelMRIDataset
 
 # FIXME: configs
 parser = HyperOptArgumentParser(strategy='random_search')
@@ -18,7 +19,7 @@ args.cg_max_iter = 2
 args.eps = 1e-6
 args.fully_sampled = True
 args.stdev = 1e-3
-args.data_file = 'data/dataset_unittest.h5'
+args.data_file = 'deepinpy/tests/dataset_unittest.h5'
 args.num_data_sets=2
 args.inverse_crime = False
 args.solver = 'adam'
@@ -34,14 +35,25 @@ args.network = 'ResNet'
 args.latent_channels = 4
 args.num_blocks = 2
 args.batch_norm = False
+args.use_sigpy = False
+args.noncart = False
+args.abs_loss = False
+args.self_supervised = False
+args.random_name = False
+args.hyperopt = False
+args.config = None
+args.distributed_training = False
+args.num_epochs = 1
+args.version = 1
 
 class TestRecon(unittest.TestCase):
 
     def test_recon(self):
-        for recon in [CGSenseRecon, MoDLRecon, DeepBasisPursuitRecon]:
+        args.Dataset = MultiChannelMRIDataset
+        for recon in [CGSenseRecon, MoDLRecon, ResNetRecon, DeepBasisPursuitRecon]:
             print('Testing Recon', recon)
             M = recon(args)
-            trainer = Trainer(max_nb_epochs=1, gpus=[3], default_save_path='deepinpy/tests/logs')
+            trainer = Trainer(max_epochs=args.num_epochs, gpus=[0], logger=False)
             trainer.fit(M)
 
 if __name__ == '__main__':
