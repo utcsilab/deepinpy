01b3fea5dcfae8d811de8704625ac0f041fe6204
tallam99 <tarekyallam99@gmail.com>
2020-05-13 15:15:38 -0500

    Add documentation to conjgrad.py

--- deepinpy/opt/conjgrad.py
+++ deepinpy/opt/conjgrad.py
@@ -6,6 +6,28 @@ from deepinpy.utils import utils
 from deepinpy.opt import ip_batch, dot_batch
 
 class ConjGrad(torch.nn.Module):
+    """A class which implements conjugate gradient descent as a torch module.
+
+    This implementation of conjugate gradient descent works as a standard torch module, with the functions forward
+    and get_metadata overridden. It is used as an optimization block within a Recon object.
+
+    Args:
+        rhs (Tensor): The residual vector b in some conjugate gradient descent algorithms.
+        Aop_fun (func): A function performing the A matrix operation.
+        max_iter (int): Maximum number of times to run conjugate gradient descent.
+        l2lam (float): The L2 lambda, or regularization parameter (must be positive).
+        eps (float): Determines how small the residuals must be before termination.
+        verbose (bool): If true, prints extra information to the console.
+
+    Attributes:
+        rhs (Tensor): The residual vector, b in some conjugate gradient descent algorithms.
+        Aop_fun (func): A function performing the A matrix operation.
+        max_iter (int): The maximum number of times to run conjugate gradient descent.
+        l2lam (float): The L2 lambda regularization parameter.
+        eps (float): Minimum residuals for termination.
+        verbose (bool): Whether or not to print extra info to the console.
+    """
+
     def __init__(self, rhs, Aop_fun, max_iter=20, l2lam=0., eps=1e-6, verbose=True):
         super(ConjGrad, self).__init__()
 
@@ -19,17 +41,45 @@ class ConjGrad(torch.nn.Module):
         self.num_cg = None
 
     def forward(self, x):
+        """Performs one forward pass through the conjugate gradient descent algorithm.
+
+        Args:
+            x (Tensor): The input to the gradient algorithm.
+
+        Returns:
+            The forward pass on x.
+
+        """
         x, num_cg = conjgrad(x, self.rhs, self.Aop_fun, max_iter=self.max_iter, l2lam=self.l2lam, eps=self.eps, verbose=self.verbose)
         self.num_cg = num_cg
         return x
 
     def get_metadata(self):
+        """Accesses metadata for the algorithm.
+
+        Returns:
+            A dict containing metadata.
+        """
+
         return {
                 'num_cg': self.num_cg,
                 }
 
 def conjgrad(x, b, Aop_fun, max_iter=10, l2lam=0., eps=1e-4, verbose=True):
-    ''' batched conjugate gradient descent. assumes the first index is batch size '''
+    """A function that implements batched conjugate gradient descent; assumes the first index is batch size.
+
+    Args:
+    	x (Tensor): The initial input to the algorithm.
+    b (Tensor): The residual vector
+    Aop_fun (func): A function performing the A matrix operation.
+    max_iter (int): Maximum number of times to run conjugate gradient descent.
+    l2lam (float): The L2 lambda, or regularization parameter (must be positive).
+    eps (float): Determines how small the residuals must be before termination…
+    verbose (bool): If true, prints extra information to the console.
+
+    Returns:
+    	A tuple containing the updated vector x and the number of iterations performed.
+    """
 
     # explicitly remove r from the computational graph
     r = b.new_zeros(b.shape, requires_grad=False)
