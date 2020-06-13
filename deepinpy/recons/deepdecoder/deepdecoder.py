8e7ae065f3d5e878c3d516209123b5c42c950d23
Ajil Jalal <ajiljalal@gmail.com>
2020-06-13 16:35:38 -0500

    minor

--- deepinpy/recons/deepdecoder/deepdecoder.py
+++ deepinpy/recons/deepdecoder/deepdecoder.py
@@ -1,45 +1,54 @@
 from deepinpy.forwards import MultiChannelMRI
-from deepinpy.models import DeepDecoder
+from deepinpy.models import decodernw
 from deepinpy.recons import Recon
 import torch
 
 class DeepDecoderRecon(Recon):
 
-	def __init__(self, args):
+    def __init__(self, args):
         self.num_channels_up = [128]*6
-		self.x_adj = None
-		#output_size = self.A.img_shape
-		super(CSDIPRecon, self).__init__(args)
-		# self.denoiser = DeepDecoder(ngf=64, output_size=[320, 256], nc=2, num_measurements=256)
-        self.denoiser = DeepDecoder(num_output_channels=2,num_channels_up=self.num_channels_up, upsample_first=True, need_sigmoid=False) #todo: check whether I need to use sigmoid
-        if self.cpu:
+        self.x_adj = None
+        super(DeepDecoderRecon, self).__init__(args)
+
+        self.denoiser = decodernw(num_output_channels=2,num_channels_up=self.num_channels_up, upsample_first=True, need_sigmoid=False) #todo: check whether I need to use sigmoid
+
+
+        # warning: don't use self.cpu, it's a function in deepdecoder
+        self.use_cpu = args.cpu
+        if self.use_cpu:
             pass
         else:
-            self.denoiser = self.denoiser.type(torch.cuda.FloatTensor)
-	def batch(self, data):
-		maps = data['maps']
-		masks = data['masks']
-		inp = data['out'] #read in maps, masks, and k-space input
+            self.denoiser = self.denoiser.to('cuda:0') # todo
+
+    def batch(self, data):
+        maps = data['maps']
+        masks = data['masks']
+        inp = data['out'] #read in maps, masks, and k-space input
 
         #initialize z
         total_upsample = 2**(len(self.num_channels_up))
         if total_upsample > 64:
             raise ValueError('desired output size of [320,256] is incompatible with more than 64x upsampling')
-		zseed = torch.zeros(self.batch_size,self.num_channels_up[0],320//total_upsample,256//total_upsample)
-		if self.cpu == True:
-			zseed.data.normal_().type(torch.FloatTensor) #init random input seed
-		else:
-			zseed.data.normal_().type(torch.cuda.FloatTensor)
-			zseed = zseed.to('cuda:0')
+        zseed = torch.zeros(self.batch_size,self.num_channels_up[0],320//total_upsample,256//total_upsample)
+        if self.use_cpu :
+            zseed.data.normal_().type(torch.FloatTensor) #init random input seed
+        else:
+            zseed.data.normal_().type(torch.cuda.FloatTensor)
+            zseed = zseed.to('cuda:0') # todo
 
-		self.z = zseed
-		self.A = MultiChannelMRI(maps, masks, l2lam=0.,  img_shape=data['imgs'].shape, use_sigpy=self.use_sigpy, noncart=self.noncart)
+        self.z = zseed
+        self.A = MultiChannelMRI(maps, masks, l2lam=0.,  img_shape=data['imgs'].shape, use_sigpy=self.use_sigpy, noncart=self.noncart)
+        if self.use_cpu :
+            pass
+        else:
+            self.A = self.A.cuda() # todo
 
-	def forward(self, y):
-		out =  self.denoiser(self.z)
-		return out
+    def forward(self, y):
+        out =  self.denoiser(self.z)
+        out = out.permute(0,2,3,1)
+        return out
 
-	def get_metadata(self):
-		return {}
+    def get_metadata(self):
+        return {}
 
 
