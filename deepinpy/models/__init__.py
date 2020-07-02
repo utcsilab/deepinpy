21f7e9ce1d962fe67ba26c6c33130d76ccae728b
Jon Tamir <jtamir@utexas.edu>
2020-07-02 18:41:47 -0500

    rename csdip model to dcgan

--- deepinpy/models/__init__.py
+++ deepinpy/models/__init__.py
@@ -7,5 +7,5 @@ A Model object transforms a variable z to a new variable w
 
 from .resnet.resnet import ResNet5Block, ResNet
 from .unroll.unroll import UnrollNet
-from .csdip.csdip import DCGAN_MRI
+from .dcgan.dcgan import DCGAN_MRI
 from .deepdecoder.deepdecoder import decodernw
