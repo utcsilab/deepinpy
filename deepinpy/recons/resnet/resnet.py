fc85ea423c970df74f8c29183a3317b5195eddcf
Jon Tamir <jtamir@utexas.edu>
2020-08-18 23:48:02 -0500

    fix multidim for ResNet5Block

--- deepinpy/recons/resnet/resnet.py
+++ deepinpy/recons/resnet/resnet.py
@@ -9,19 +9,19 @@ class ResNetRecon(Recon):
 
     def __init__(self, hparams):
         super(ResNetRecon, self).__init__(hparams)
+
+        copy_shape = np.array(self.D.shape)
+        if hparams.num_spatial_dimensions == 2:
+            num_channels = 2*np.prod(copy_shape[1:-2])
+        elif hparams.num_spatial_dimensions == 3:
+            num_channels = 2*np.prod(copy_shape[1:-3])
+        else:
+            raise ValueError('only 2D or 3D number of spatial dimensions are supported!')
+        self.in_channels = num_channels
         
         if self.hparams.network == 'ResNet5Block': # FIX ALSO
-            self.network = ResNet5Block(num_filters=self.hparams.latent_channels, filter_size=7, batch_norm=self.hparams.batch_norm)
+            self.network = ResNet5Block(num_filters_start=self.in_channels, num_filters_end=self.in_channels, num_filters=self.hparams.latent_channels, filter_size=7, batch_norm=self.hparams.batch_norm)
         elif self.hparams.network == 'ResNet':
-            copy_shape = np.array(self.D.shape)
-            if hparams.num_spatial_dimensions == 2:
-                num_channels = 2*np.prod(copy_shape[1:-2])
-            elif hparams.num_spatial_dimensions == 3:
-                num_channels = 2*np.prod(copy_shape[1:-3])
-            else:
-                raise ValueError('only 2D or 3D number of spatial dimensions are supported!')
-            self.in_channels = num_channels
-            
             self.network = ResNet(in_channels=self.in_channels, latent_channels=self.hparams.latent_channels, num_blocks=self.hparams.num_blocks, kernel_size=7, batch_norm=self.hparams.batch_norm)
 
     def batch(self, data):
