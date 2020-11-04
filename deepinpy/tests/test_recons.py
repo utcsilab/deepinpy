6bad3a6d6fecca546bb340d19c63418844ced97e
Jon Tamir <jtamir@utexas.edu>
2020-11-04 00:15:35 -0600

    tests: add more args to Trainer

--- deepinpy/tests/test_recons.py
+++ deepinpy/tests/test_recons.py
@@ -46,6 +46,8 @@ args.num_epochs = 2
 args.lr_scheduler = -1
 args.checkpoint_init = None
 args.num_spatial_dimensions = 2
+args.num_accumulate = 1
+args.clip_grad = 0
 
 class TestRecon(unittest.TestCase):
 
@@ -56,7 +58,7 @@ class TestRecon(unittest.TestCase):
 
             print('  CPU:')
             M = recon(args)
-            trainer = Trainer(max_epochs=args.num_epochs, gpus=None, logger=False)
+            trainer = Trainer(max_epochs=args.num_epochs, gpus=None, logger=False, accumulate_grad_batches=args.num_accumulate, progress_bar_refresh_rate=1, gradient_clip_val=args.clip_grad)
             trainer.fit(M)
 
             if torch.cuda.device_count() > 0:
